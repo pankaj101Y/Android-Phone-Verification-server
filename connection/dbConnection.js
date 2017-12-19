f5292ead7007843dce7ae8ed754112e310a214a1
pankaj101Y <pnkcomm2u@gmail.com>
2017-12-19 21:34:18 +0530

    set up for heroku

--- connection/dbConnection.js
+++ connection/dbConnection.js
@@ -4,6 +4,6 @@ mongoose.Promise=global.Promise;
 const dbURI="mongodb://one:wqe@Aw5q3qdsf@ds161426.mlab.com:61426/mobile_verify";
 
 //'mongodb://localhost:27017/OpenApp'
-mongoose.connect(dbURI);
+mongoose.connectUri(dbURI);
 
 module.exports={mongoose};
