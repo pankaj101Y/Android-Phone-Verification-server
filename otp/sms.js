3b5c7da87fdc5022735ba1b5f20c96792e834213
pankaj101Y <pnkcomm2u@gmail.com>
2017-12-21 10:40:46 +0530

    id

--- otp/sms.js
+++ otp/sms.js
@@ -1,4 +1,4 @@
 const authKey="188443Aul4hIOppm5a35f219";
-const sms="{{otp}} is your OTP for verification.##OpenApp";
+const sms="{{otp}} is your OTP for verification. ##OpenApp";
 
 module.exports={authKey,sms};
